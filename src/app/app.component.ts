955f694e6230697278ca422930f188203d997e11
israel <israel.bejarano.a@gmail.com>
2019-04-04 22:07:26 +0200

    solved code warnings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,10 @@ import { WebsocketService } from './services/websocket.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   constructor(public wsService: WebsocketService) {
 
   }
 
-  ngOnInit() {
-    
-  }
+  ngOnInit() {}
 }
