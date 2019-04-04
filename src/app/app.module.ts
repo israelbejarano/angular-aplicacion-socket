74d8bf63aa0d9c804867f8e0cb194cb6e998b96a
israel <israel.bejarano.a@gmail.com>
2019-04-04 23:19:31 +0200

    importaciones necesarias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { FormsModule } from '@angular/forms';
+
 
 // sockets
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
@@ -9,14 +11,17 @@ const config: SocketIoConfig = { url: environment.wsUrl, options: {} };
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ChatComponent } from './components/chat/chat.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     SocketIoModule.forRoot(config)
   ],
   providers: [],
