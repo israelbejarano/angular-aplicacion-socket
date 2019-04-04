6f4a0cbf739b28a7749f46198d3abaa5fb3b2893
israel <israel.bejarano.a@gmail.com>
2019-04-04 23:18:17 +0200

    chat component para enviar mms al servidor

---
+++ src/app/components/chat/chat.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ChatService } from 'src/app/services/chat.service';
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.css']
+})
+export class ChatComponent implements OnInit {
+
+  texto = '';
+
+  constructor(public chatService: ChatService) { }
+
+  enviar() {
+    console.log(this.texto);
+    this.chatService.sendMensaje(this.texto);
+    this.texto = '';
+  }
+
+  ngOnInit() {
+  }
+
+}
