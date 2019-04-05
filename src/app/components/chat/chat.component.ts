f859239bc30dd977f8715876239fbd7c3e27bbc3
israel <israel.bejarano.a@gmail.com>
2019-04-05 20:14:52 +0200

    pintar mms de manera dinamica

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,24 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ChatService } from 'src/app/services/chat.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit, OnDestroy {
 
   texto = '';
+  mensajesSubscription: Subscription;
+  mensajes: any[] = [];
+  elemento: HTMLElement;
 
   constructor(public chatService: ChatService) { }
 
   enviar() {
+    if (this.texto.trim().length === 0) {
+      return;
+    }
     console.log(this.texto);
     this.chatService.sendMensaje(this.texto);
     this.texto = '';
   }
 
   ngOnInit() {
+    this.elemento = document.getElementById('chat-mensajes');
+    this.mensajesSubscription = this.chatService.getMensajes().subscribe(msg => {
+      console.log(msg);
+      this.mensajes.push(msg);
+      setTimeout(() => {
+        this.elemento.scrollTop = this.elemento.scrollHeight;
+      }, 50);
+    });
+  }
+
+  ngOnDestroy() {
+    this.mensajesSubscription.unsubscribe();
   }
 
 }
--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -15,4 +15,8 @@ export class ChatService {
     };
     this.wsService.emit('mensaje', payload);
   }
+
+  getMensajes() {
+    return this.wsService.listen('mensaje-nuevo');
+  }
 }
