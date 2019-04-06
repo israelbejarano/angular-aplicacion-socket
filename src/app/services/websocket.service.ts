147d0086bf2ac7236e7445a84afc58b7e0fbb32e
israel <israel.bejarano.a@gmail.com>
2019-04-06 13:18:58 +0200

    mantener usuarios en back

--- src/app/services/websocket.service.ts
+++ src/app/services/websocket.service.ts
@@ -64,6 +64,7 @@ export class WebsocketService {
   cargarStorage() {
     if (localStorage.getItem('usuario')) {
       this.usuario = JSON.parse(localStorage.getItem('usuario'));
+      this.loginWS(this.usuario.nombre); // mantener en el back los usuarios
     }
   }
 }
