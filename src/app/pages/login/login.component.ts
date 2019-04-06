85459b52cb75817fd1cc76aee19633c3fb4a08dd
israel <israel.bejarano.a@gmail.com>
2019-04-06 12:55:12 +0200

    redireccion a mensajes una vez logeado

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { WebsocketService } from 'src/app/services/websocket.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,14 +11,17 @@ export class LoginComponent implements OnInit {
 
   nombre = '';
 
-  constructor(public wsService: WebsocketService) { }
+  constructor(public wsService: WebsocketService, private router: Router) { }
 
   ngOnInit() {
   }
 
   ingresar() {
     console.log(this.nombre);
-    this.wsService.loginWS(this.nombre);
+    this.wsService.loginWS(this.nombre)
+      .then(() => {
+        this.router.navigateByUrl('/mensajes');
+      });
   }
 
 }
