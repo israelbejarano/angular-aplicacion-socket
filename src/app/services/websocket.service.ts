1b791c469ff8b69d792d74e32a630fc52375d80f
israel <israel.bejarano.a@gmail.com>
2019-04-07 11:46:54 +0200

    logout de la app

--- src/app/services/websocket.service.ts
+++ src/app/services/websocket.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Socket } from 'ngx-socket-io';
 import { Usuario } from '../classes/usuario';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class WebsocketService {
   public socketStatus = false;
   public usuario: Usuario;
 
-  constructor(private socket: Socket) {
+  constructor(private socket: Socket, private router: Router) {
     this.cargarStorage();
     this.checkStatus();
   }
@@ -49,6 +50,17 @@ export class WebsocketService {
         resolve();
       });
     });
+  }
+
+  // logout
+  logoutWS() {
+    this.usuario = null;
+    localStorage.removeItem('usuario');
+    const payload = {
+      nombre: 'sin-nombre'
+    }
+    this.emit('configurar-usuario', payload, () => {});
+    this.router.navigateByUrl('/');
   }
 
   // obtener usuario
